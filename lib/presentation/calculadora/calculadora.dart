f3a32f5e25e5a61879e3af1129b343563dd5501e
Francisco Santin <franciscosantin2002@gmail.com>
2023-10-13 22:01:13 -0300

    Cambios de apariencia

--- lib/presentation/calculadora/calculadora.dart
+++ lib/presentation/calculadora/calculadora.dart
@@ -33,117 +33,111 @@ class Calculadora extends StatelessWidget {
                 width: 7.0,
                 color: colors.primary,
               )),
-          width: double.infinity,
-          height: 500,
-          child: GridView.count(
-              padding: const EdgeInsets.only(left: 300),
-              scrollDirection: Axis.horizontal,
-              mainAxisSpacing: 10.0,
-              crossAxisSpacing: 10.0,
-              crossAxisCount: 4,
-              children: [
-                CalculatorButton(
-                    content: '7',
-                    func: () {
-                      displayProvider.displayPrint('7');
-                    }),
-                CalculatorButton(
-                    content: '4',
-                    func: () {
-                      displayProvider.displayPrint('4');
-                    }),
-                CalculatorButton(
-                    content: '1',
-                    func: () {
-                      displayProvider.displayPrint('1');
-                    }),
-                CalculatorButton(
-                    content: '0',
-                    func: () {
-                      displayProvider.displayPrint('0');
-                    }),
-                CalculatorButton(
-                    content: '8',
-                    func: () {
-                      displayProvider.displayPrint('8');
-                    }),
-                CalculatorButton(
-                    content: '5',
-                    func: () {
-                      displayProvider.displayPrint('5');
-                    }),
-                CalculatorButton(
-                    content: '2',
-                    func: () {
-                      displayProvider.displayPrint('2');
-                    }),
-                CalculatorButton(
-                    content: '.',
-                    func: () {
-                      displayProvider.displayPrint('.');
-                    }),
-                CalculatorButton(
-                    content: '9',
-                    func: () {
-                      displayProvider.displayPrint('9');
-                    }),
-                CalculatorButton(
-                    content: '6',
-                    func: () {
-                      displayProvider.displayPrint('6');
-                    }),
-                CalculatorButton(
-                    content: '3',
-                    func: () {
-                      displayProvider.displayPrint('3');
-                    }),
-                CalculatorButton(
-                    content: 'EXP',
-                    func: () {
-                      displayProvider.displayPrint('EXP');
-                    }),
-                CalculatorButton(
-                    content: 'DEL',
-                    func: () {
-                      displayProvider.displayDel();
-                    }),
-                CalculatorButton(
-                    content: 'X',
-                    func: () {
-                      displayProvider.displayPrint('*');
-                    }),
-                CalculatorButton(
-                    content: '+',
-                    func: () {
-                      displayProvider.displayPrint('+');
-                    }),
-                CalculatorButton(
-                    content: 'ANS',
-                    func: () {
-                      String auxAns = displayProvider.ans.toString();
-                      displayProvider.displayPrint(auxAns);
-                    }),
-                CalculatorButton(
-                    content: 'AC',
-                    func: () {
-                      displayProvider.displayReset();
-                    }),
-                CalculatorButton(
-                    content: '/',
-                    func: () {
-                      displayProvider.displayPrint('/');
-                    }),
-                CalculatorButton(
-                    content: '-',
-                    func: () {
-                      displayProvider.displayPrint('-');
-                    }),
-                CalculatorButton(
-                    content: '=',
-                    func: () {
-                      displayProvider.displayPrintResult();
-                    }),
-              ]),
+          width: 480,
+          height: 829,
+          child: GridView.count(crossAxisCount: 5, children: [
+            CalculatorButton(
+                content: '7',
+                func: () {
+                  displayProvider.displayPrint('7');
+                }),
+            CalculatorButton(
+                content: '8',
+                func: () {
+                  displayProvider.displayPrint('8');
+                }),
+            CalculatorButton(
+                content: '9',
+                func: () {
+                  displayProvider.displayPrint('9');
+                }),
+            CalculatorButton(
+                content: 'DEL',
+                func: () {
+                  displayProvider.displayDel();
+                }),
+            CalculatorButton(
+                content: 'AC',
+                func: () {
+                  displayProvider.displayReset();
+                }),
+            CalculatorButton(
+                content: '4',
+                func: () {
+                  displayProvider.displayPrint('4');
+                }),
+            CalculatorButton(
+                content: '5',
+                func: () {
+                  displayProvider.displayPrint('5');
+                }),
+            CalculatorButton(
+                content: '6',
+                func: () {
+                  displayProvider.displayPrint('6');
+                }),
+            CalculatorButton(
+                content: 'X',
+                func: () {
+                  displayProvider.displayPrint('*');
+                }),
+            CalculatorButton(
+                content: '/',
+                func: () {
+                  displayProvider.displayPrint('/');
+                }),
+            CalculatorButton(
+                content: '1',
+                func: () {
+                  displayProvider.displayPrint('1');
+                }),
+            CalculatorButton(
+                content: '2',
+                func: () {
+                  displayProvider.displayPrint('2');
+                }),
+            CalculatorButton(
+                content: '3',
+                func: () {
+                  displayProvider.displayPrint('3');
+                }),
+            CalculatorButton(
+                content: '+',
+                func: () {
+                  displayProvider.displayPrint('+');
+                }),
+            CalculatorButton(
+                content: '-',
+                func: () {
+                  displayProvider.displayPrint('-');
+                }),
+            CalculatorButton(
+                content: '0',
+                func: () {
+                  displayProvider.displayPrint('0');
+                }),
+            CalculatorButton(
+                content: '.',
+                func: () {
+                  displayProvider.displayPrint('.');
+                }),
+            CalculatorButton(
+                content: 'EXP',
+                func: () {
+                  displayProvider.displayPrint('EXP');
+                }),
+            CalculatorButton(
+                content: 'ANS',
+                func: () {
+                  String auxAns = displayProvider.ans.toString();
+                  displayProvider.displayPrint(auxAns);
+                }),
+            CalculatorButton(
+                content: '=',
+                func: () {
+                  displayProvider.displayPrintResult();
+                }),
+          ]),
         ),
       ],
     );
